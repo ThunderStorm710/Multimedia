94a7e62267642b0b165e30d0f87a85628f581462
Thunder <pascensao77@gmail.com>
2023-04-29 03:07:59 +0100

    Ex.3 Feito

--- TP2/mrs.py
+++ TP2/mrs.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import os
 from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import skew, kurtosis
+from scipy.spatial.distance import cosine
 
 
 def lerFicheiroCsv(fich: str):
@@ -27,61 +28,74 @@ def normalizarFeatures(info):
 
 
 def extrairFeatures():
-    # features_list = np.empty(10,)
     features_list = []
     i = 0
 
-    if not "stats_features_librosa.npy" in os.listdir():
-        features_list = np.load("stats_features_librosa.npy", allow_pickle=True)
-        print("TIPO", type(features_list.astype(list)))
-        print("TIPO", features_list)
-        return features_list.astype(list)
+    if "Features - 900x190.npy" in os.listdir():
+        features_list = np.load("Features - 900x190.npy", allow_pickle=True)
+        return features_list
     else:
         for filename in os.listdir(f"MER_audio_taffc_dataset/Songs"):
+            '''
             if i == 20:
                 break
+            '''
             print(f"FICHEIRO --> {filename}")
 
             if filename.endswith('.mp3'):
                 y, sr = librosa.load("MER_audio_taffc_dataset/Songs/" + filename, sr=22050, mono=True)
 
-                mfcc = librosa.feature.mfcc(y=y, sr=sr)
-                mfcc = mfcc[:13, :]
+                mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+                lista = calcularStats(mfcc)
                 spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
+                lista.extend(calcularStats(spectral_centroid))
                 spectral_bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+                lista.extend(calcularStats(spectral_bandwidth))
                 spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr)
+                lista.extend(calcularStats(spectral_contrast))
                 spectral_flatness = librosa.feature.spectral_flatness(y=y)
+                lista.extend(calcularStats(spectral_flatness))
                 spectral_rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+                lista.extend(calcularStats(spectral_rolloff))
                 f0 = librosa.yin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'))
                 f0 = f0.reshape(1, f0.shape[0])
+                lista.extend(calcularStats(f0))
                 rms = librosa.feature.rms(y=y)
+                lista.extend(calcularStats(rms))
                 zero_crossing_rate = librosa.feature.zero_crossing_rate(y=y)
+                lista.extend(calcularStats(zero_crossing_rate))
                 tempo = librosa.beat.tempo(y=y, sr=sr)
-                '''
-                a = np.array([mfcc, spectral_centroid, spectral_bandwidth, spectral_contrast, spectral_flatness, spectral_rolloff, f0, rms, zero_crossing_rate, tempo])
-                features_list = np.insert(features_list,  [a], axis=0)
-                print(features_list.shape)
-                print(len(features_list[0]))
-                for l in features_list[0]:
-                    print(l)
-                    '''
+                lista.extend(tempo)
 
-                features_list.append([mfcc, spectral_centroid, spectral_bandwidth, spectral_contrast, spectral_flatness,
-                                      spectral_rolloff, f0, rms, zero_crossing_rate, tempo])
+                features_list.append(lista)
                 i += 1
-        print(len(features_list), len(features_list[0][0]))
-        # print(features_list.shape)
+        lista = np.array(features_list)
+        np.save("Features - 900x190", lista)
+        np.savetxt("Features - 900x190.csv", features_list, delimiter=",")
         return features_list
 
 
+def calcularStats(dados):
+    listaAux = []
+    for j in dados:  # CADA FEATURE
+        mean = np.mean(j)
+        std = np.std(j)
+        skewness = skew(j)
+        curtose = kurtosis(j)
+        median = np.median(j)
+        valorMax = np.max(j)
+        valorMin = np.min(j)
+
+        listaAux.extend([mean, std, skewness, curtose, median, valorMax, valorMin])
+
+    return listaAux
+
+
 def calcularEstatisticas(dados):
-    # listaDados = np.empty(shape=(0,))
     listaDados = []
 
     for i in dados:  # MUSICAS
         listaAux = []
-        contador = 0
-        aux = 0
         for j in i:  # CADA FEATURE
 
             shape = j.shape
@@ -97,8 +111,6 @@ def calcularEstatisticas(dados):
                     valorMin = np.min(k)
 
                     listaAux.extend([mean, std, skewness, curtose, median, valorMax, valorMin])
-                    contador += 7
-                    aux += 1
             else:
                 mean = np.mean(j)
                 std = np.std(j)
@@ -109,32 +121,15 @@ def calcularEstatisticas(dados):
                 valorMin = np.min(j)
 
                 listaAux.extend([mean, std, skewness, curtose, median, valorMax, valorMin])
-                contador += 7
-                aux += 1
 
-        # listaDados = np.append(listaDados, listaAux, axis=0)
         listaDados.append(listaAux)
 
     print("DADOS = ", len(listaDados), len(listaDados[0]))
-    np.save('stats_features_librosa', listaDados)
     print("NORMALIZAR")
     normalizar(listaDados)
     print("-----------------------------------")
-    # np.savetxt("Estatisticas.txt", listaDados)
 
     return listaDados
-
-
-def obterMusicas():
-    listaMusicas = []
-    for ficheiro in os.listdir(f"MER_audio_taffc_dataset/Songs"):
-        if ficheiro.endswith('.mp3'):
-            y, sr = librosa.load("MER_audio_taffc_dataset/Songs/" + ficheiro, sr=22050, mono=True)
-            print(len(y))
-            listaMusicas.append(y)
-
-    #print("FIM = ", len(listaMusicas))
-    return listaMusicas
 
 
 def obterNomesMusicas():
@@ -149,93 +144,82 @@ def obterNomesMusicas():
 def normalizar(lista):
     array = np.array(lista)
     scaler = MinMaxScaler(feature_range=(0, 1))
-    for i in range(len(lista)):
-        #print(i, " VALOR")
+    for i in range(len(lista[0])):
         aux = array[:, i]  # usa a sintaxe do NumPy para acessar a coluna do array
-        #print(aux)
         aux = scaler.fit_transform(aux.reshape(-1, 1))  # redimensiona a coluna para ter formato adequado
         array[:, i] = aux.flatten()  # atualiza a coluna normalizada no array
-        #print(aux)
 
-    #print(len(array))
-    np.save("ArrayNormalizado", array)
+    np.save("Features Normalizadas 900x190", array)
+    np.savetxt("Features Normalizadas - 900x190.csv", array, delimiter=",")
+
     return array
 
 
-def obterDistancias(lista):
-    # lista = obterMusicas()
-    distanciaEuclidiana = []
-    distanciaManhattan = []
-    distanciaCosseno = []
+def obterDistancias(lista, featuresTop):
+    distanciaEuclidiana = np.zeros((900, 900))
+    distanciaManhattan = np.zeros((900, 900))
+    distanciaCosseno = np.zeros((900, 900))
+    distanciaEuclidianaTop100 = np.zeros((900, 900))
+    distanciaManhattanTop100 = np.zeros((900, 900))
+    distanciaCossenoTop100 = np.zeros((900, 900))
 
     for i in range(len(lista)):
-        auxE = []
-        auxM = []
-        auxC = []
-        for j in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            aux1 = np.array(lista[i])
+            aux2 = np.array(lista[j])
+            distanciaEuclidiana[i][j] = distanciaEuclidiana[j][i] = np.linalg.norm(aux1 - aux2)
+            distanciaManhattan[i][j] = distanciaManhattan[j][i] = np.sum(np.abs(aux1 - aux2))
+            distanciaCosseno[i][j] = distanciaCosseno[j][i] = cosine(aux1, aux2)
+
+    for i in range(len(featuresTop)):
+        for j in range(i, len(featuresTop)):
             if i == j:
-                auxE.append(-1.0)
-                auxM.append(-1.0)
-                auxC.append(-1.0)
+                distanciaEuclidianaTop100[i][j] = distanciaEuclidianaTop100[j][i] = -1
+                distanciaManhattanTop100[i][j] = distanciaManhattanTop100[j][i] = -1
+                distanciaCossenoTop100[i][j] = distanciaCossenoTop100[j][i] = -1
             else:
-                der = euclidean_distance(lista[i][0], lista[j][0])
-                dmr = manhattan_distance(lista[i][0], lista[j][0])
-                dcr = cosine_similarity(lista[i][0], lista[j][0])
-                auxE.append(der)
-                auxM.append(dmr)
-                auxC.append(dcr)
-        distanciaEuclidiana.append(auxE)
-        distanciaManhattan.append(auxM)
-        distanciaCosseno.append(auxC)
-    #print(len(distanciaEuclidiana), len(distanciaEuclidiana[0]))
-    #print(distanciaEuclidiana)
+                aux1 = np.array(featuresTop[i])
+                aux2 = np.array(featuresTop[j])
+                distanciaEuclidianaTop100[i][j] = distanciaEuclidianaTop100[j][i] = np.linalg.norm(aux1 - aux2)
+                distanciaManhattanTop100[i][j] = distanciaManhattanTop100[j][i] = np.sum(np.abs(aux1 - aux2))
+                distanciaCossenoTop100[i][j] = distanciaCossenoTop100[j][i] = cosine(aux1, aux2)
+
     np.save("der", distanciaEuclidiana)
     np.save("dmr", distanciaManhattan)
     np.save("dcr", distanciaCosseno)
 
+    np.savetxt("der.csv", distanciaEuclidiana, delimiter=",")
+    np.savetxt("dmr.csv", distanciaManhattan, delimiter=",")
+    np.savetxt("dcr.csv", distanciaCosseno, delimiter=",")
 
-def euclidean_distance(x1, x2):
-    return np.sqrt(np.sum((x1 - x2) ** 2))
+    np.save("derTop100", distanciaEuclidianaTop100)
+    np.save("dmrTop100", distanciaManhattanTop100)
+    np.save("dcrTop100", distanciaCossenoTop100)
+
+    np.savetxt("derTop100.csv", distanciaEuclidianaTop100, delimiter=",")
+    np.savetxt("dmrTop100.csv", distanciaManhattanTop100, delimiter=",")
+    np.savetxt("dcrTop100.csv", distanciaCossenoTop100, delimiter=",")
+
+    return distanciaEuclidiana
 
 
-def manhattan_distance(x1, x2):
-    return np.sum(np.abs(x1 - x2))
-
-
-def cosine_similarity(x1, x2):
-    dot_product = np.dot(x1, x2)
-    norm_x1 = np.sqrt(np.sum(x1 ** 2))
-    norm_x2 = np.sqrt(np.sum(x2 ** 2))
-    return dot_product / (norm_x1 * norm_x2)
-
-
-def rankingSimilaridade(ficheiro, info):
-    dir = os.listdir("Queries/")
-    listaEuclidiana = []
+def rankingSimilaridade(info):
+    diretoria = os.listdir("Queries/")
     musicas = obterNomesMusicas()
-    if ficheiro in dir:
-        y, sr = librosa.load("Queries/" + ficheiro, sr=22050, mono=True)
-        mfcc = librosa.feature.mfcc(y=y, sr=sr)
-        mfcc = mfcc[0]
-        mean = np.mean(mfcc)
-        info = info[0:20]
+    for ficheiro in diretoria:
+        if ficheiro.endswith(".mp3"):
+            listaMusicas = []
+            indice = musicas.index(ficheiro)  # Indice da musica/query
+            linha = info[indice]  # Distancia entre a query e todas as musicas
+            array = np.array(linha)
+            array = np.argsort(array)[0:20]
 
-        for i in info:
-            der = euclidean_distance(i[0], mean)
-            listaEuclidiana.append(der)
+            for i in array:
+                listaMusicas.append(musicas[i])
+            print(f"Query {ficheiro} --> {listaMusicas}")
 
-        array = np.array(listaEuclidiana)
-        array = np.argsort(array)
-        print(array)
-        listaMusicas = []
-        for i in array:
-            listaMusicas.append(musicas[i])
-        print(listaMusicas)
-        return array
-
-
-    else:
-        print("Query não encontrada...")
+        else:
+            print(f"Query {ficheiro} não encontrada...")
 
 
 if __name__ == "__main__":
@@ -247,15 +231,15 @@ if __name__ == "__main__":
 
     # --- Play Sound
     # sd.play(y, sr, blocking=False)
-    features = lerFicheiroCsv('Features - Audio MER/top100_features.csv')
-    featuresNormalizadas = normalizarFeatures(features)
+    featuresTop100 = lerFicheiroCsv('Features - Audio MER/top100_features.csv')
+    featuresNormalizadasTop100 = normalizarFeatures(featuresTop100)
 
     features = extrairFeatures()
-    stats = calcularEstatisticas(features)
-    # normalizarFeatures(stats)
+    # stats = calcularEstatisticas(features)
+    featuresNormalizadas = normalizar(features)
 
-    obterDistancias(stats)
-    rankingSimilaridade("MT0000202045.mp3", features)
+    der = obterDistancias(features, featuresTop100)
+    rankingSimilaridade(der)
 
     # --- Plot sound waveform
     # plt.figure()
